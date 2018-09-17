6fcdc71d01e90fdabef3d99aa4bea8833868366b
QoroviqiDiar <qoroviqidiar@gmail.com>
2018-09-17 20:48:28 +0200

    Registerint user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\User;
+use function bcrypt;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+
+    public function register(StoreUserRequest $request)
+    {
+
+        $user = new User;
+
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+
+        $user->save();
+        
+    }
 }
